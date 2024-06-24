1df1ef1f1d8087d1153ca1e40e864ec69869122f
Alessandra Campos <alessandra.campos1983@gmail.com>
2024-06-23 23:38:53 -0300

    first commit

--- workout_api/routers.py
+++ workout_api/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter # type: ignore
 from workout_api.atleta.controller import router as atleta
 from workout_api.categorias.controller import router as categorias
 from workout_api.centro_treinamento.controller import router as centro_treinamento
